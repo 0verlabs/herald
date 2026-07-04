57cb47cee023d6808cf71f06d3472b85ca12111f
Lucky Ivanius <luckzivanius@gmail.com>
2026-07-04 19:28:28 +0700

    refactor(chat): remove astryx

--- packages/chat/src/routes/__root.tsx
+++ packages/chat/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import "../styles.css";
 
 export const Route = createRootRoute({
@@ -6,21 +6,5 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
-  return (
-    <>
-      <div className="p-2 flex gap-2 text-4xl">
-        <Link
-          to="/"
-          activeProps={{
-            className: "font-bold",
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
