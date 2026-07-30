a05057dec8a3daf90187e093523d51be264c5903
Lucky Ivanius <luckzivanius@gmail.com>
2026-07-31 02:05:27 +0700

    feat(api): circle wallet integration

--- packages/api/src/handlers/auth.ts
+++ packages/api/src/handlers/auth.ts
@@ -1,23 +1,78 @@
+import { initiateDeveloperControlledWalletsClient } from "@circle-fin/developer-controlled-wallets";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
+import { userWallets } from "@ivanius.ai/db";
+
 import type { ClerkWebhookVariables } from "../middlewares/clerk";
+import { createDb } from "../lib/db";
 import { clerkWebhookSecret } from "../middlewares/clerk";
-import { ok } from "../utils/response";
+import { ok, unexpectedError } from "../utils/response";
 
 const auth = new Hono<{ Bindings: Env; Variables: ClerkWebhookVariables }>().post(
   "/webhook",
   clerkWebhookSecret(),
   async (c) => {
-    const event = c.get("clerkWebhookEvent");
+    try {
+      const event = c.get("clerkWebhookEvent");
 
-    if (event.type !== "user.created") {
+      if (event.type !== "user.created") {
+        return ok(c);
+      }
+
+      const db = createDb(c.env.DB);
+      const walletClient = initiateDeveloperControlledWalletsClient({
+        apiKey: c.env.CIRCLE_API_KEY,
+        entitySecret: c.env.CIRCLE_ENTITY_SECRET,
+      });
+
+      const userId = event.data.id;
+
+      const [existingUserWallet] = await db
+        .select({
+          circleWalletId: userWallets.circleWalletId,
+        })
+        .from(userWallets)
+        .where(eq(userWallets.userId, userId));
+      if (existingUserWallet) {
+        const existingCircleWallet = await walletClient.getWallet({
+          id: existingUserWallet.circleWalletId,
+        });
+        if (existingCircleWallet) return ok(c);
+      }
+
+      const createWalletResponse = await walletClient.createWallets({
+        accountType: "EOA",
+        blockchains: ["EVM-TESTNET"],
+        walletSetId: c.env.CIRCLE_WALLET_SET_ID,
+        count: 1,
+        metadata: [{ name: "Agent Wallet", refId: userId }],
+      });
+
+      if (!createWalletResponse.data) {
+        console.error("[Circle] Failed to create user wallet:", createWalletResponse.status);
+
+        return unexpectedError(c);
+      }
+
+      const [wallet] = createWalletResponse.data.wallets;
+      if (!wallet) {
+        console.error("[Circle] Wallet didn't returned");
+
+        return unexpectedError(c);
+      }
+
+      await db
+        .insert(userWallets)
+        .values({ userId: event.data.id, network: "evm", circleWalletId: wallet.id })
+        .onConflictDoNothing();
+
       return ok(c);
+    } catch (err) {
+      console.error(err);
+
+      return unexpectedError(c);
     }
-
-    // const db = createDb(c.env.DB);
-    // await db.insert(userWallets).values({ userId: event.data.id }).onConflictDoNothing();
-
-    return ok(c);
   }
 );
 
